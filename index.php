5010412db67fb3d087934fb920403ff6e85148f1
Yuze Wu <1204074991@qq.com>
2017-09-17 16:19:52 +0800

    Fixed bug of index.php
    由于本人的疏忽，将未完工项目 Signer 的主页面放入此项目，故紧急修复

--- index.php
+++ index.php
@@ -7,26 +7,27 @@
 	if(!defined("title")){
 		define("title","MeowChat - A Public Chat System");
 	};
-?><!DOCTYPE HTML>
+?>
+<!DOCTYPE HTML>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
 		<title><?php echo title ?></title>
 		<link rel="stylesheet" href="css/buttons.css"/>
-	<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/bootstrap.css"/>
 	</head>
 	<body style="background-color:#2588cb;color:#fff;text-align:center;padding:0;margin:0;overflow:hidden;" scroll="no">
-	<div id="body">
-		<img style="margin-top:10%;height:140px;width:140px;background-color:white;" src="favicon.ico"/>
+	<center>
+		<img style="margin-top:10%;height:140px;weigh:140px;background-color:white;border-radius: 15px;" src="favicon.ico"/>
 		<h1>Meow-Chat</h1>
-		<form action="chat.php">
+   <form class="form-signin" action="chat.php" style="width:300px;">
 		<div class="input-group">
   <span class="input-group-addon">用户名</span>
-      <input type="text" class="form-control" name="username" id="username" placeholder="您的用户名" maxlength="16" minlength="4">
-</div>
-        <button class="button button-3d button-primary button-rounded" type="sumbit">进入</button>
+  <input id="username" name="username" type="text" class="form-control" minlength="3" maxlength="12" <?php if(!empty($_GET['username'])){echo 'value="'.$_GET['username'].'"';};?> required placeholder="聊天用户名"/>
+  </div>
+		<button type="submit" class="button button-3d button-primary button-rounded">进入</button>
         </form>
-		</div>
+		</center>
 	</body>
 </html>
